fb3b5a6391d67598c45c621c316c5b373bd86fba
github username <github email address>
2019-11-30 12:11:18 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,12 @@
 module ArtistsHelper
+
+    def display_artist(song)
+        unless song.artist
+            text = "<a href='/songs/#{song.id}/edit'>Add Artist</a>"
+        else
+            text = "<a href='/artists/#{song.artist.id}'>#{song.artist.name}</a>"
+        end
+        
+        text
+    end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,16 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    result = self.artist.name if self.artist
   end
 
   def artist_name=(name)
+    @artist = Artist.find_by(name: name)
+    if @artist
+      @artist.update(name: name)
+      self.artist = @artist
+    else
+      self.artist = Artist.create(name: name)
+    end
   end
 end
